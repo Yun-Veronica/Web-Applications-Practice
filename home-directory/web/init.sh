b3a36ec9b218a46d7021b576e2cf750f3ef9dcc3
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-10-27 18:33:01 +0600

    update init.sh
    added lines for restarting gunicorn

--- home-directory/web/init.sh
+++ home-directory/web/init.sh
@@ -1,2 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+# restarting gunicorn
+sudo /etc/init.d/gunicorn restart
+gunicorn --bind='0.0.0.0:8080' hello:wsgi_app
+
+
