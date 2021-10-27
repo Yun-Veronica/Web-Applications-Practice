91783628886ecee8c6b22256d8fb43b37d878fb9
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-10-27 19:07:10 +0600

    create hello.py

---
+++ home-directory/web/hello.py
@@ -0,0 +1,12 @@
+# WSGI application return document with MIME type text/plain document, containing all GET parameters, one for each line.
+# For example, when requesting "/?a=1&a=2&b=3", the application should return the following text:
+# a=1
+# a=2
+# b=3
+
+def wsgi_app(environ,start_response):
+    status = "200 OK"
+    headers = [('Content-Type','text/plain')]
+    body = [ bytes(i + '\n', 'ascii')for i in environ['QUERY_STRING'].split('&') ]
+    start_response(status, headers)
+    return [body]
