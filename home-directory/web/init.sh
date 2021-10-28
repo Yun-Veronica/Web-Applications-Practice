9e6fb3393d11acf3f4de3099f55e6112ecf22323
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-10-28 19:32:31 +0600

    Update init.sh

--- home-directory/web/init.sh
+++ home-directory/web/init.sh
@@ -1,4 +1,6 @@
+# restarting nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+rm -rf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 # restarting gunicorn
