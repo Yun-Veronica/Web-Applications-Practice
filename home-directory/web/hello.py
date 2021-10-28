cd56176babc01b54f2be70372f56f9f9ff55cdae
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-10-28 21:31:23 +0600

    Update hello.py

--- home-directory/web/hello.py
+++ home-directory/web/hello.py
@@ -7,6 +7,6 @@
 def wsgi_app(environ,start_response):
     status = "200 OK"
     headers = [('Content-Type','text/plain')]
-    body = [ bytes(i + '\n', 'ascii')for i in environ['QUERY_STRING'].split('&') ]
+    body = [ (bytes(i, 'ascii') + b'\n') for i in environ['QUERY_STRING'].split('&') ]
     start_response(status, headers)
     return [body]
