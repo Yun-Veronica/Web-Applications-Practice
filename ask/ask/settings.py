18c5fcc503199f6c6e4c84032ab6247bcf8c6107
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-02 21:30:46 +0600

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+k=^&uk3@3u^abc57#p#=6-zdnx74iw%$4&hmd-l4#4n#p@kf%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
