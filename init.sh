25d02e50c3861ba56210cb73b7885a0b701d7f2f
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-05 17:28:56 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ sudo rm -rf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 # restarting gunicorn
-sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/gunicorn restart
 
 
 #  gunicorn [OPTIONS] [WSGI_APP]
 #  path to wsgi app looks like this: folder.file-without-extension:function_name
 gunicorn --bind='0.0.0.0:8000' web.hello:wsgi_app
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test
-gunicorn --bind='0.0.0.0:8080' web.ask.ask.wsgi:application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf web.ask.ask.wsgi:application
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test.conf
+# gunicorn --bind='0.0.0.0:8080' web.ask.ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf web.ask.wsgi:application
 
