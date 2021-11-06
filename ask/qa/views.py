9c6738b907e2827d151ae7b1c58557aff7453b3e
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-06 11:56:54 +0600

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,6 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request,*args, **kwargs):
-    response_body = 'Answer is 42'.encode('utf-8')
-    start_response('200 OK',  [('Content-type','text/plain')])
-    return  iter ([response_body]), HttpResponse('OK')
+    #response_body = b'Answer is 42'.encode('utf-8')
+    return HttpResponse('Answer is 42', headers={ 'Content-Type': 'text/plain', 'status_code':200})
