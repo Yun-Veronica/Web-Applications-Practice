d3538c8126dec020906fe5c120a29a9706136f21
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-06 21:29:59 +0600

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url,patterns
 from django.contrib import admin
-from . import views
+from qa.views import test
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', 'admin.site.urls'),
 #     url(r'^ş', 'views.test', name='home'),
-    re_path(r'^/$', 'views.test', name='home'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^questions/\d+/$','qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test'),
+    url(r'^/$', 'test', name='home'),
+    url(r'^login/.*$', 'test'),
+    url(r'^signup/.*$', 'test'),
+    url(r'^questions/\d+/$','test'),
+    url(r'^ask/.*$', 'test'),
+    url(r'^popular/.*$', 'test'),
+    url(r'^new/.*$', 'test')
 ]
