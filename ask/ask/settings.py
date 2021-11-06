14d17bf23c27761a37f9f274d443721c4c1be091
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-06 22:07:28 +0600

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'web.ask.qa'
+    'web.ask.qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
