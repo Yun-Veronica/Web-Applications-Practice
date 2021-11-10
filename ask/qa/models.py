20c32708882d0f11d1cd9ee6ecfe9238863eb04b
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-10 19:18:03 +0600

    Update models.py
    created Question, Answer models 
    created QuestionManager model manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+user = django.contrib.auth.models.User
+
+class Question(models.Model):
+  title = models.CharField(max_length=255)
+  text = models.TextField()
+  added_at = models.DateTimeField()
+  rating = models.IntegerField()
+  author = models.CharField(max_length=50)
+  likes  = models.ManytoManyField(user)
+  
+  def __unicode__(self):
+    return self.title
+  
+  def get_absolute_url(self):
+    return '/question/%d/' %self.pk
+    
+  def questionID(self):
+    return  self.pk
+    
+
+class Answer(models.Model):
+  text = models.TextField()
+  added_at = models.DateTimeField()
+  author = models.CharField(max_length=50)
+  question = models.OnetoOneField(questionID)
+   
+    def __unicode__(self):
+    return self.title
+  
+  def get_absolute_url(self):
+    return '/answer/%d/' %self.pk
+
+
+class QuestionManager(models.Manager):
+  def new(self):
+    return self.order_by('added_at')[:20]
+    
+  def popular(self):
+    return self.order_by('rating')
