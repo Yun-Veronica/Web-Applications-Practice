d9d908a32fd48aaa58e16c047243ba6b7e0a97d9
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-10 21:02:53 +0600

    Update bd_init.sh

--- bd_init.sh
+++ bd_init.sh
@@ -6,4 +6,4 @@ mysql -uroot -e "create user 'box'@'localhost' identified by '1234';"
 mysql -uroot -e "grant all privileges on qadb.* to 'box'@'localhost' with grant option;"
 
 cd ~/web/ask && python3 manage.py makemigrations qa
-&& python3 manage.py migrate
+cd ~/web/ask && python3 manage.py migrate
