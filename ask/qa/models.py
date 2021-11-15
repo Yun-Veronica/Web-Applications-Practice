1085c1f2821253b812fac89b186b0f8e565d7a02
Yun-Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2021-11-15 17:15:15 +0600

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
   def new(self):
-    return self.order_by('-pk')
-    
+    return self.order_by('-added_at')
+  
   def popular(self):
     return self.order_by('-rating')
 
